055ea35f2c6debe7e0f5736a138da59fcc40d455
Björn Carlsson <b.r.carlsson@gmail.com>
2013-01-19 08:59:50 +0100

    refactored to use rows and columns

--- gtk/Main.cs
+++ gtk/Main.cs
@@ -7,6 +7,8 @@ namespace gtkmine
 	{
 		public static void Main (string[] args)
 		{
+			int rows = 5;
+			int columns = 5;
 			Application.Init ();
 			Window win = new Window ("Minesweeper");
 			win.WindowPosition = ((global::Gtk.WindowPosition)(4));
@@ -14,13 +16,15 @@ namespace gtkmine
 			var table = new global::Gtk.Table (((uint)(5)), ((uint)(5)), false);
 			table.RowSpacing = ((uint)(6));
 			table.ColumnSpacing = ((uint)(6));
-			for (uint i = 0; i < 25; i++) {
-				var button = new Button();
-				button.CanFocus = true;
-				button.Label = i.ToString();
-				button.Clicked += new EventHandler(OnButtonClicked);
-				table.Attach(button, i%5, i%5+1,i/5, i/5+1);
-				table.Add(button);
+			for (uint row = 0; row < rows; row++) {
+				for (uint column = 0; column < columns; column++) {
+					var button = new Button();
+					button.CanFocus = true;
+					button.Label = (row + column + row * column).ToString();
+					button.Clicked += new EventHandler(OnButtonClicked);
+					table.Attach(button, row, row+1,column, column+1);
+					table.Add(button);
+				}
 			}
 			table.ShowAll();
 			win.Add(table);
