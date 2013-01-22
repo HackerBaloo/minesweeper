cb44c76000e3bd063853e41d0abc7dc56e75baa4
Björn Carlsson <b.r.carlsson@gmail.com>
2013-01-22 19:22:34 +0100

    Made a simplistic version which works with Monodevelop on Linux

--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,7 +11,28 @@ namespace WindowsForms
   {
     public Form1()
     {
-      InitializeComponent();
+      var button = new System.Windows.Forms.Button();
+      SuspendLayout();
+      // 
+      // button1
+      // 
+      button.Location = new System.Drawing.Point(13, 24);
+//      button1.Name = "button1";
+      button.Size = new System.Drawing.Size(23, 23);
+      button.TabIndex = 0;
+      button.Text = "*";
+      //button1.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      ClientSize = new System.Drawing.Size(284, 262);
+      Controls.Add(button);
+      Name = "Form1";
+      Text = "Minesweeper";
+      ResumeLayout(false);
+
     }
   }
 }
